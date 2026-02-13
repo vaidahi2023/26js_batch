896efab090c14061ad7243fa83140a7685d0a52a
vaidahi2023 <vaidahi810@gmail.com>
2026-02-13 17:48:29 +0530

    json

---
+++ json/json1.js
@@ -0,0 +1,30 @@
+//javascript object notation
+
+// object name={
+//     key:value
+// }
+
+var student={
+    id:1,
+    name:"riya",
+    email:"riya12@gmail.com",
+    address:{
+        street:"vijay park" ,
+        city:"surat",
+        state:"Gujarat"
+    }
+}
+
+//console.log(student)
+console.log(student.id)
+console.log(student.name)
+console.log(student.email)
+
+console.log(student.address)
+console.log(student.address.city)
+console.log(student.address.street)
+console.log(student.address.state)
+
+
+
+
---
+++ json/json2.js
@@ -0,0 +1,58 @@
+// var phone ={
+
+//     name:"iphone",
+//     price:2000,
+//     details:{
+//         camera:"30mp",
+//         Storage:"12gb",
+//         ram:"4gb"
+//     },
+//     apps:['whatsapp','facebook','google']
+// }
+
+// console.log(phone)
+// console.log(phone.name)
+// console.log(phone.price)
+// console.log(phone.apps)
+// console.log(phone.apps[1])
+
+var phone =[
+{
+
+    name:"iphone",
+    price:2000,
+    details:{
+        camera:"30mp",
+        Storage:"12gb",
+        ram:"4gb"
+    },
+    apps:['whatsapp','facebook','google']
+},{
+
+    name:"samsung",
+    price:20000,
+    details:{
+        camera:"40mp",
+        Storage:"12gb",
+        ram:"6gb"
+    },
+    apps:['whatsapp','facebook','google']
+},
+{
+    name:"opp",
+    price:2000,
+    details:{
+        camera:"30mp",
+        Storage:"12gb",
+        ram:"4gb"
+    },
+    apps:['whatsapp','facebook','google']
+}
+]
+
+console.log(phone)
+console.log(phone[0])
+console.log(phone[1])
+console.log(phone[2])
+
+
