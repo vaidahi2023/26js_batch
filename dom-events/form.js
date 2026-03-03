a234958f709a81a69db8f0933bff3ac8cb3891b8
vaidahi2023 <vaidahi810@gmail.com>
2026-03-03 17:23:16 +0530

    form

---
+++ dom-events/form.js
@@ -0,0 +1,16 @@
+const submitHandler=(event)=>{
+
+    event.preventDefault();
+
+    var name=document.getElementById("name")
+    console.log(name.value)
+
+    var age=document.getElementById("age")
+    console.log(age.value)
+
+    var color=document.getElementById("color")
+    console.log(color.value)
+
+    var nameoutput=document.getElementById("nameo")
+    
+}
